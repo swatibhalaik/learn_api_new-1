dc2eb5542e382eb8dfda55bac7ef207f8e7329ac
Kalicharan Singh Sikarwar <Kalicharan.S@coforgetech.com>
2021-09-11 16:33:01 +0530

    mongo db connection

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
+import uvicorn
+from pymongo import MongoClient
 
 app = FastAPI()
+
+DB = "api_test"
+
+EMP_COLLECTION = "emp"
+
+
 
 class Info(BaseModel):
     name: str
@@ -16,7 +24,11 @@ def read_root():
 
 @app.post("/emp/")
 def create_item(info: Info):
-    return info
+    with MongoClient() as client:
+        msg_collection = client[DB][EMP_COLLECTION]
+        result = msg_collection.insert_one(info.dict())
+        ack = result.acknowledged
+        return {"insertion": ack}
 
 @app.get("/emp/")
 def read_item(emp_id: int, remarks: Optional[str] = None):
@@ -25,3 +37,9 @@ def read_item(emp_id: int, remarks: Optional[str] = None):
 @app.put("/emp/")
 def update_info(remarks: Optional[str] = None):
     return {"updated_remarks": remarks}
+
+
+if __name__ == "__main__":
+    # for testing purpose reload=True
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    # for production and deployment reload =False
