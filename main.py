a71ea52db8668c61a9908c6032450b3ae307f5e1
swatibhalaik <87295632+swatibhalaik@users.noreply.github.com>
2021-09-12 10:55:51 +0530

    Tried to implement functinality to get and put
    Get is not displaying stored info
    Put giving an error

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
+import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
-import uvicorn
 from pymongo import MongoClient
 
 app = FastAPI()
 
+
 DB = "api_test"
+
 
 EMP_COLLECTION = "emp"
 
 
-
 class Info(BaseModel):
     name: str
+    emp_id: int
     loc: str
     is_working_from_home: Optional[bool] = None
 
@@ -22,21 +24,34 @@ class Info(BaseModel):
 def read_root():
     return {"Message": "Hello Employee"}
 
-@app.post("/emp/")
+
+@app.post("/Info/")
 def create_item(info: Info):
     with MongoClient() as client:
-        msg_collection = client[DB][EMP_COLLECTION]
-        result = msg_collection.insert_one(info.dict())
+        emp_collection = client[DB][EMP_COLLECTION]
+        result = emp_collection.insert_one(info.dict())
         ack = result.acknowledged
         return {"insertion": ack}
 
-@app.get("/emp/")
-def read_item(emp_id: int, remarks: Optional[str] = None):
-    return {"emp_id": emp_id, "remarks": remarks}
 
-@app.put("/emp/")
-def update_info(remarks: Optional[str] = None):
-    return {"updated_remarks": remarks}
+@app.get("/Info/{emp_id}", response_model=list[Info])
+def read_item(emp_id: int):
+    with MongoClient() as client:
+        emp_collection = client[DB][EMP_COLLECTION]
+        emp_list = emp_collection.find({"emp_id": emp_id})
+        response_emp_list =[]
+        for emp in emp_list:
+            response_emp_list.append(Info(**emp))
+        return response_emp_list
+
+
+@app.put("/Info/{emp_id}",response_model=Info)
+def update_info(emp_id: int, loc: str):
+    with MongoClient() as client:
+        emp_collection = client[DB][EMP_COLLECTION]
+        result = emp_collection.update({"emp_id": emp_id}, {"loc": loc})
+        ack = result.acknowledged
+        return {"Updated": ack}
 
 
 if __name__ == "__main__":
